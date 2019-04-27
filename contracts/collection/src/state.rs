448d870889351e1b51c55fbf0cd9d2db9b852d94
BabyDragon003 <golddragon01234@gmail.com>
2019-04-27 13:09:32 +0900

    "2019-04-27 13:09"

--- contracts/collection/src/state.rs
+++ contracts/collection/src/state.rs
@@ -23,12 +23,3 @@ pub struct Config {
 }
 
 
-
-pub const CONFIG_KEY: &str = "config";
-pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
-
-pub const SALE_KEY: &str = "sale";
-pub const SALE: Map<String, SaleInfo> = Map::new(SALE_KEY);
-// pub const PRICE_KEY: &str = "price";
-// pub const PRICE: Map<u32, Uint128> = Map::new(PRICE_KEY);
-
--- contracts/nftsale/src/msg.rs
+++ contracts/nftsale/src/msg.rs
@@ -18,16 +18,11 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-pub enum QueryMsg {
-    GetConfig {},
-    GetToken {
-        index: u32
-    }
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct ConfigResponse {
-    pub owner: Addr,
+    UpdateOwner {
+        owner: Addr,
+    },
+    UpdateEnabled {
+        enabled: bool
     pub price: Uint128,
     pub total_count: u32,
     pub sold_index: u32,
